eab024c050b25e611a1f7365c9d0dec3afcd0a1e
zaphir <ianice.lange@supdevinci-edu.fr>
2025-03-09 12:56:10 +0100

    Refactor application name and update assets; remove unused play icon files and enhance track update functionality with additional metadata.

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 const { app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path');
-let lastTrack = "";  // Déclarer ici pour qu'il soit accessible partout
 const RPC = require('discord-rpc');
 const client = new RPC.Client({ transport: 'ipc' });
 
-const clientId = '1348067895879532554'; // Remplace par ton Client ID
+const clientId = ''; // Your application client ID here
+
 
 let mainWindow;
-let isPaused = false; // Flag pour savoir si la musique est en pause
-let lastElapsedTime; // Dernier temps écoulé
 client.on('ready', () => {
 app.whenReady().then(() => {
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
+        icon: "./assets/deezer.png",
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js') // Spécifiez le fichier de preload ici
+            preload: path.join(__dirname, 'preload.js')
         }
     });
-
+    mainWindow.setMenuBarVisibility(false);
     mainWindow.webContents.setUserAgent(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     );
 
     mainWindow.loadURL('https://www.deezer.com/');
     
-    // Fonction qui surveille les changements de titre de musique
+   
     function trackChanges() {
-        console.log('trackChanges appelé');
         mainWindow.webContents.executeJavaScript(`
             (function() {
                 function startObserver() {
                     const targetNode = document.querySelector('.css-1d4hfp6');
                     if (!targetNode) {
-                        console.log("Aucun élément 'player' trouvé, tentative de nouveau dans 1 seconde");
-                        setTimeout(startObserver, 1000);  // Essayer à nouveau après 1 seconde
+                        setTimeout(startObserver, 1000);
                         return;
                     }
 
-                    console.log("Élément 'player' trouvé", targetNode);
+                    
 
                     const observer = new MutationObserver(() => {
                         const title = document.querySelector('[data-testid="item_title"]')?.innerText || 'Inconnu';
@@ -48,50 +45,41 @@ app.whenReady().then(() => {
                         const elapsedTime = document.querySelector('[data-testid="elapsed_time"]')?.innerText || 'Inconnu';
                         const remainingTime = document.querySelector('[data-testid="remaining_time"]')?.innerText || 'Inconnu';
                         const image = document.querySelector('[data-testid="item_cover"] img')?.src || 'Inconnu';
+                        const urlMusic = document.querySelector('[data-testid="item_title"] a')?.href || 'https://www.deezer.com/';
 
-                        console.log("Changement détecté:", { title, artist, elapsedTime, remainingTime, image });
+                        
 
-                        window.electronAPI.trackUpdate({ title, artist, elapsedTime, remainingTime, image });
+                        window.electronAPI.trackUpdate({ title, artist, elapsedTime, remainingTime, image, urlMusic });
                     });
 
                     observer.observe(targetNode, { childList: true, subtree: true, characterData: true });
                 }
 
-                startObserver();  // Démarrer l'observateur
+                startObserver(); 
             })();
         `);
     }
 
-    let isPaused = false; // Flag pour savoir si la musique est en pause
-let lastTrack = "";
-
 ipcMain.on("trackUpdate", (event, trackInfo) => {
-    if (lastTrack !== trackInfo.title) {
-        console.log('🎵 Nouveau titre :', trackInfo.title);
-        console.log('🎤 Artiste :', trackInfo.artist);
-        console.log('🖼️ Image :', trackInfo.image);
-        lastTrack = trackInfo.title;
-    }
-
-    console.log(trackInfo.elapsedTime + ' / ' + trackInfo.remainingTime);
-    if(trackInfo.elapsedTime === lastElapsedTime) {
-        isPaused = true;
-    }
-    else {
-        isPaused = false;
-    }
-
+    
     const elapsed_time = timeToMilliseconds(trackInfo.elapsedTime);
     const remaining_time = timeToMilliseconds(trackInfo.remainingTime);
-    
+    const dateNow = Date.now();
         client.setActivity({
             details: trackInfo.title,
-            state: trackInfo.artist,
             largeImageKey: trackInfo.image,
+            largeImageText: trackInfo.artist,
+            smallImageText: "Deezer",
+            smallImageKey: 'https://cdn-files.dzcdn.net/cache/images/common/favicon/favicon.5e8e3be4042b873a7358.ico',
             instance: false,
-            type: 2,
-            startTimestamp: Date.now() - elapsed_time,
-            endTimestamp: Date.now() + remaining_time
+            startTimestamp: dateNow - elapsed_time,
+            endTimestamp: dateNow + remaining_time - elapsed_time,
+            buttons: [
+                {
+                    label: 'Listen on Deezer',
+                    url: `${trackInfo.urlMusic}`
+                }
+            ]
         });
     
 });
@@ -103,7 +91,7 @@ ipcMain.on("trackUpdate", (event, trackInfo) => {
 });
 
 client.login({ clientId }).catch((err) => {
-    console.error('Erreur de connexion :', err);
+    console.error('Error :', err);
 });
 function timeToMilliseconds(timeStr) {
     const [minutes, seconds] = timeStr.split(':').map(Number);
